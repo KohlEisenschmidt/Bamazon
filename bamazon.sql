810089a31331461422fe85734b640d9c2367c288
Kohl Eisenschmidt <kohleisenschmidt@gmail.com>
2018-03-28 22:48:25 -0400

    got initial prompts working. need to implement the .then from responses

--- bamazon.sql
+++ bamazon.sql
@@ -13,5 +13,25 @@ CREATE TABLE products (
     PRIMARY KEY (id)
 );
 
-SELECT * FROM Top5000
+SELECT * FROM products
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("ball", "sport", 5, 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("bat", "sport", 10, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("cup", "kitchen", 5, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("spoon", "kitchen", 7, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("soap", "bath", 2, 20);
+
+-- INSERT INTO Top5000 (artistName, songName, releseYear, worldRating,
+--             USRating, UKRating, europeRating, restofworldRating)
+-- VALUES("Bing Crosby", "White Christmas",
+--  1942, 39.903, 23.929, 5.7, 2.185, 0.54  
+-- );
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,29 +15,119 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  runSearch();
+    // console.log("connected as id " + connection.threadId);
+  
+    connection.query("SELECT * FROM products", function(err, results){
+      if(err) throw err;
+      console.log(results);
+      connection.end();
+    })
+
+
+  displayAndAsk();
 });
-function runSearch() {
+function displayAndAsk() {
     inquirer
       .prompt({
         name: "action",
         type: "list",
-        message: "What would you like to do?",
+        message: "Would you like to buy a product",
         choices: [
-          "What product would you like to buy?(select item number)",
-          "How many units of the product would you like to purchaces"
+          "Yes",
+          "No. Just browsing."
         ]
       })
       .then(function(answer) {
         switch (answer.action) {
-        case "What product would you like to buy?(select item number)":
+        case "Yes":
           productToBuy();
           break;
   
-        case "How many units of the product would you like to purchaces":
-          howManyToBuy();
+        case "No. Just browsing":
+          declinedSale();
           break;
-  
         }
       });
-  }
+    } 
+
+function declinedSale(){
+    if (err) throw err;
+    console.log("Let me know if you need any help.\n");
+    // inquirer
+    // .prompt({
+    //     message: "Let me know if you need any help."
+    //   })
+    displayAndAsk();
+}
+
+function productToBuy(){
+    inquirer
+    .prompt({
+        name: "id",
+        type: "input",
+        message: "Which ID of the product you would like to buy"
+      })
+    
+    .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].id < 0) {
+            inquirer
+            .prompt({
+                name: "id",
+                type: "input",
+                message: "How many units of the product would you like to buy"
+              });
+          }
+        }
+
+    //     // determine if bid was high enough
+    //     if (chosenItem.highest_bid < parseInt(answer.bid)) {
+    //       // bid was high enough, so update db, let the user know, and start over
+    //       connection.query(
+    //         "UPDATE auctions SET ? WHERE ?",
+    //         [
+    //           {
+    //             highest_bid: answer.bid
+    //           },
+    //           {
+    //             id: chosenItem.id
+    //           }
+    //         ],
+    //         function(error) {
+    //           if (error) throw err;
+    //           console.log("Bid placed successfully!");
+    //           start();
+    //         }
+    //       );
+    //     }
+    //     else {
+    //       // bid wasn't high enough, so apologize and start over
+    //       console.log("Your bid was too low. Try again...");
+    //       start();
+    //     }
+    //   });
+    // }
+    // inquirer
+    //   .prompt({
+    //     name: "action",
+    //     type: "list",
+    //     message: "What would you like to do?",
+    //     choices: [
+    //       "What product would you like to buy?(select item number)",
+    //       "How many units of the product would you like to purchaces"
+    //     ]
+    //   })
+    //   .then(function(answer) {
+    //     switch (answer.action) {
+    //     case "What product would you like to buy?(select item number)":
+    //       productToBuy();
+    //       break;
+  
+    //     case "How many units of the product would you like to purchaces":
+    //       howManyToBuy();
+    //       break;
+    //     }
+      });
+    }
